54492900853310b0db4b2b6a232d084a9864bb89
vande <vande@ec2-54-232-189-113.sa-east-1.compute.amazonaws.com>
2026-05-06 18:49:59 -0300

    Sincronizando meus exercicios de POO

---
+++ ExercicioPOOSobrecarga/src/main/java/com/ufba/exerciciopoosobrecarga/Academia.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ufba.exerciciopoosobrecarga;
+
+/**
+ *
+ * @author vande
+ */
+public class Academia {
+    public 
+    public int mensalidade;
+    
+}
